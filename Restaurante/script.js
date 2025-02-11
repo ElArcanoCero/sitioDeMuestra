5de2e4d7e1492c2ec409007c9f85e8ed84c1167e
ElArcanoCero <128997205+ElArcanoCero@users.noreply.github.com>
2025-02-11 17:12:03 -0500

    modified:   Restaurante/index.html 	modified:   Restaurante/script.js 	modified:   Restaurante/style.css

--- Restaurante/script.js
+++ Restaurante/script.js
@@ -11,26 +11,24 @@ var swiper = new Swiper(".mySwiper-1",{
         prevEl:".swiper-button-prev",
     }
 
-})
-var swiper = new Swiper(".mySwiper-2",{
+});
+var swiper2 = new Swiper(".mySwiper-2", {
     slidesPerView: 3,
     spaceBetween: 20,
     loop: true,
-    loopFillGroupWithBlank:true,
-    navigation:{
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
+    navigation: {
+        nextEl: ".swiper-button-next-2",
+        prevEl: ".swiper-button-prev-2",
     },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
         },
-        520:{
-            slidesPerView:2,
+        520: {
+            slidesPerView: 2,
         },
-        950:{
-            slidesPerView:3,
+        950: {
+            slidesPerView: 3,
         }
     }
-
-})
+});
