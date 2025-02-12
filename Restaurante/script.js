177f18de49cd18bd21fbba83c9aceb43f514cbb0
ElArcanoCero <128997205+ElArcanoCero@users.noreply.github.com>
2025-02-11 20:03:50 -0500

    new file:   Restaurante/apuntes 	modified:   Restaurante/index.html 	modified:   Restaurante/script.js 	modified:   Restaurante/style.css

--- Restaurante/script.js
+++ Restaurante/script.js
@@ -16,19 +16,29 @@ var swiper2 = new Swiper(".mySwiper-2", {
     slidesPerView: 3,
     spaceBetween: 20,
     loop: true,
+    loopFillGroupWithBlank:true,
     navigation: {
-        nextEl: ".swiper-button-next-2",
-        prevEl: ".swiper-button-prev-2",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints: {
         0: {
-            slidesPerView: 1,
+            slidesPerView: 1
         },
         520: {
-            slidesPerView: 2,
+            slidesPerView: 2
         },
         950: {
-            slidesPerView: 3,
+            slidesPerView: 3
         }
     }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+tabInputs.forEach(function(input){
+    input.addEventListener('change', function(){
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+    })
 });
